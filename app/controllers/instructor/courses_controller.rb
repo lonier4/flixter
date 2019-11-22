9299d7ffa71de357518d0127b5a8bd36ba5ecc01
Andre Yeargins <lonier4@gmail.com>
2019-11-22 21:08:37 +0000

    troubleshooting image and video uploader. Added lines to carrierwave.rb to fix it

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_user, only: [:show]
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -21,7 +21,7 @@ class Instructor::CoursesController < ApplicationController
 
 private
 
-  def require_authorized_for_current_user
+  def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
@@ -33,6 +33,6 @@ private
   end
 
   def course_params
-    params.require(:course).permit(:title, :description, :cost)
+    params.require(:course).permit(:title, :description, :cost, :image,)
   end
 end
