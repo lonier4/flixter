21a07b6e778b9720003ff333252947719d0614a4
Andre Yeargins <lonier4@gmail.com>
2019-12-06 03:15:46 +0000

    added Stripe for student checkout CC payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   
@@ -6,9 +8,16 @@ class LessonsController < ApplicationController
 
   private
 
+  def require_authorized_for_current_lesson 
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You Must Be Enrolled"  
+    end
+  end
+
+
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-
 end
